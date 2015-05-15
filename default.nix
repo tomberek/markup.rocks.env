bf684f40de09c202da5b720b50a18ec19ff3afa6
Ozan Sener <ozan@ozansener.com>
2015-05-15 12:46:28 +0700

    Update pandoc and add css-text package

--- default.nix
+++ default.nix
@@ -35,6 +35,16 @@ let overrideCabal = drv: f: (drv.override (args: args // {
           sha256 = "0yn2nj6irmj24j1djvnnq26i2lbf9g9x1wdhmcrk519glcn5k64j";
           buildDepends = [ self.semigroups ] ++ drv.buildDepends; # For some reason, without the spurious import of self.semigroups, HaskellForMaths will fail to build the environment for HaskellForMaths on ghcjs (it works on ghc)
         });
+        css-text = self.mkDerivation (with self; {
+          pname = "css-text";
+          version = "0.1.2.1";
+          sha256 = "1xi1n2f0g8y43p95lynhcg50wxbq7hqfzbfzm7fy8mn7gvd920nw";
+          buildDepends = [ attoparsec base text ];
+          homepage = "http://www.yesodweb.com/";
+          description = "CSS parser and renderer";
+          license = stdenv.lib.licenses.bsd3;
+          doCheck = false;
+        });
       };
     };
 in rec {
